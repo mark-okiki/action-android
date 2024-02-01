309870c7f94cb282bf1d550ef5b1bcda556f75d0
mark-okiki <mark.okiki@zoopla.co.uk>
2024-02-01 11:38:16 +0000

    Using qemu-system-aarch64-headless emulator

--- emulator-run-cmd/lib/sdk.js
+++ emulator-run-cmd/lib/sdk.js
@@ -157,8 +157,9 @@ class BaseAndroidSdk {
                 yield (0, exec_1.exec)(`cd ${this.qemuPath()}`);
                 this.createHyperVisorEntitlement();
                 yield (0, exec_1.exec)(`du -h entitlements.xml`);
-                yield (0, exec_with_result_1.execIgnoreFailure)(`codesign -s - --entitlements entitlements.xml --force qemu-system-aarch64;
-            codesign -s - --entitlements entitlements.xml --force qemu-system-aarch64-headless;`);
+                yield (0, exec_1.exec)(`ls`);
+                yield (0, exec_with_result_1.execIgnoreFailure)(`codesign -s - --entitlements entitlements.xml --force qemu-system-aarch64-headless;
+            codesign -s - --entitlements entitlements.xml --force qemu-system-aarch64;`);
                 yield (0, exec_1.exec)(`cd -`);
                 return true;
             }
